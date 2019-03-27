47ed5f598ec5879a6330badf6b7fab6f7aac6d67
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-03-27 14:20:16 +0100

    Updates

---
+++ precache-manifest.0dc556eddb5e03a0b36c2c54739a6812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27928ea5f10aaf7308b4",
+    "url": "/Weather-app/static/css/main.566fa0a9.chunk.css"
+  },
+  {
+    "revision": "27928ea5f10aaf7308b4",
+    "url": "/Weather-app/static/js/main.f248bdc5.chunk.js"
+  },
+  {
+    "revision": "ee457148f4c194dd0d10",
+    "url": "/Weather-app/static/js/runtime~main.f4357388.js"
+  },
+  {
+    "revision": "5989ac65bfc45523a95f",
+    "url": "/Weather-app/static/js/2.e3978392.chunk.js"
+  },
+  {
+    "revision": "3e0181421833f5a5a4cb7acf30d0631c",
+    "url": "/Weather-app/static/media/clouds.3e018142.jpg"
+  },
+  {
+    "revision": "86be7cff8c2d59c7ef47f0bc21a64a9d",
+    "url": "/Weather-app/index.html"
+  }
+];
